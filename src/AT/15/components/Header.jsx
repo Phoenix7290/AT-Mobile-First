baf425d614c32605c32a8849ff4df0309d0fffbf
Phoenix7290 <b5261064@gmail.com>
2024-09-27 16:40:29 -0300

    small fixes

--- src/AT/15/components/Header.jsx
+++ src/AT/15/components/Header.jsx
@@ -1,6 +1,6 @@
 import { MdForum } from "react-icons/md";
 import PropTypes from "prop-types";
-import { AppBar, Toolbar, IconButton, Typography, Button, Box } from "@mui/material";
+import { AppBar, Toolbar, IconButton, Typography, Button } from "@mui/material";
 
 export default function Header({ isMenuOpen, setIsMenuOpen }) {
   return (
@@ -16,25 +16,6 @@ export default function Header({ isMenuOpen, setIsMenuOpen }) {
           Menu
         </Button>
       </Toolbar>
-      <Box
-        component="ul"
-        sx={{
-          display: isMenuOpen ? "block" : "none",
-          backgroundColor: "white",
-          position: "absolute",
-          top: "64px",
-          left: 0,
-          right: 0,
-          listStyle: "none",
-          p: 2,
-          boxShadow: 3,
-        }}
-      >
-        <li>Perfil</li>
-        <li>Fotos</li>
-        <li>Amigos</li>
-        <li>Sugestões</li>
-      </Box>
     </AppBar>
   );
 }
